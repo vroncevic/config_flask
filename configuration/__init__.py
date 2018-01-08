332bb03140c172d753cc2c1799332a42227b1095
Vladimir Roncevic <vroncevic@debtux>
2018-01-08 18:22:53 +0100

    Updated comment-header

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -2,12 +2,12 @@
 # __init__.py
 # Copyright (C) 2018 Vladimir Roncevic <elektron.ronca@gmail.com>
 #
-# ats_utilities is free software: you can redistribute it and/or modify it
+# config_flask is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ats_utilities is distributed in the hope that it will be useful, but
+# config_flask is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
