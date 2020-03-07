000ecdaada31c41ebd90e8084e1c8fb7b61aa0dd
Vladimir Roncevic <elektron.ronca@gmail.com>
2020-03-07 21:53:47 +0100

    [config_flask] pyqc and pydoc done, updated doc

--- configuration/test_config.py
+++ configuration/test_config.py
@@ -1,22 +1,32 @@
 # -*- coding: UTF-8 -*-
-# test_config.py
-# Copyright (C) 2018 Vladimir Roncevic <elektron.ronca@gmail.com>
-#
-# config_flask is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# config_flask is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program. If not, see <http://www.gnu.org/licenses/>.
-#
 
-from app_server.configuration import BaseConfig
+"""
+ Module
+     test_config.py
+ Copyright
+     Copyright (C) 2018 Vladimir Roncevic <elektron.ronca@gmail.com>
+     config_flask is free software: you can redistribute it and/or modify it
+     under the terms of the GNU General Public License as published by the
+     Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+     config_flask is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+     See the GNU General Public License for more details.
+     You should have received a copy of the GNU General Public License along
+     with this program. If not, see <http://www.gnu.org/licenses/>.
+ Info
+     Define class TestConfig with attribute(s) and method(s).
+     Test configuration.
+"""
+
+import sys
+
+try:
+    from app_server.configuration import BaseConfig
+except ImportError as error:
+    MESSAGE = "\n{0}\n{1}\n".format(__file__, error)
+    sys.exit(MESSAGE)  # Force close python ATS ##############################
 
 __author__ = "Vladimir Roncevic"
 __copyright__ = "Copyright 2018, Free software to use and distributed it."
@@ -30,7 +40,7 @@ __status__ = "Updated"
 
 class TestConfig(BaseConfig):
     """
-        Define class DevelopmentConfig with attribute(s) and method(s).
+        Define class TestConfig with attribute(s) and method(s).
         Test configuration.
         It defines:
             attribute:
@@ -48,4 +58,3 @@ class TestConfig(BaseConfig):
     DEBUG_TB_ENABLED = True
     DEBUG_TB_INTERCEPT_REDIRECTS = True
     BCRYPT_LOG_ROUNDS = 4
-
